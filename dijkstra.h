97a42a1038b3f75435dbc152fbe764248972246b
up202305651 <up202305651@up.pt>
2025-03-29 17:31:36 +0000

    Update dijkstra.h

--- dijkstra.h
+++ dijkstra.h
@@ -9,6 +9,7 @@
 void dijkstraDriving(Graph *g, int origin);
 void displayFirst(Graph *g, int origin,int destination);
 void displayTwo(Graph *g, int origin, int destination, const vector<int>& nodesToAvoid, const vector<pair<int, int>>& edgesToAvoid, int includeVertex);
+void displayTwoFile(Graph *g, int origin, int destination, const vector<int>& nodesToAvoid,const vector<pair<int, int>>& edgesToAvoid, int includeVertex, ofstream &outputFile);
 
 
 #endif //DIJKSTRA_H
